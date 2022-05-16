4e95bb95242a9c8a271b3c8266df1d397c032988
annaclaraf <anisferraz@gmail.com>
2022-05-16 16:34:09 -0300

    cadastro funcionario page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Login } from './pages/login/index'
 import { Home } from './pages/home/index'
 import { Funcionario } from './pages/funcionario/index'
+import { FuncionarioCadastro } from './pages/funcionario/cadastrar/index'
+
 
 import './App.css'
 
@@ -13,6 +15,7 @@ function App() {
           <Route path="/home" element={<Home/>} />
           <Route path="/login" element={<Login/>} />
           <Route path="/funcionarios" element={<Funcionario/>} />
+          <Route path="/funcionarios/cadastro" element={<FuncionarioCadastro/>} />
       </Routes>
     </BrowserRouter>
  );
---
+++ src/pages/funcionario/cadastrar/index.js
@@ -0,0 +1,154 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+
+import { api } from '../../../services/api'
+
+import './style.css'
+
+import { Sidebar } from '../../../components/Sidebar/index'
+
+export function FuncionarioCadastro() {
+    const navigate = useNavigate();
+
+    const [CPF, setCPF] = useState("");
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [telefone, setTelefone] = useState("");
+    const [setor, setSetor] = useState("");
+    const [rua, setRua] = useState("");
+    const [CEP, setCEP] = useState("");
+    const [numero, setNumero] = useState("");
+    const [cidade, setCidade] = useState("");
+    const [estado, setEstado] = useState("");
+
+
+    async function handleCreate(event) {
+        event.preventDefault()
+
+        try {
+            const response = await api.post('/funcionarios', {
+                CPF,
+                nome,
+                email,
+                telefone,
+                setor,
+                rua,
+                CEP,
+                numero,
+                cidade,
+                estado
+            })
+
+            navigate('/funcionarios');
+
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    return (
+        <main>
+            <Sidebar />
+
+            <section className="cadastro">
+                <header>
+                    <h2>FUNCIONÁRIOS</h2>
+                </header>
+
+                <div>
+                    <form>
+                        <section className='form'>
+                            <div>
+                                <input
+                                    id="nome"
+                                    type="text"
+                                    placeholder="Nome Completo:"
+                                    onChange={event => setNome(event.target.value)}
+                                    required
+                                />
+
+                                <input
+                                    id="email"
+                                    type="email"
+                                    placeholder="Email:"
+                                    onChange={event => setEmail(event.target.value)}
+                                    required
+                                />
+
+                                <input
+                                    id="CPF"
+                                    type="text"
+                                    placeholder="CPF:"
+                                    onChange={event => setCPF(event.target.value)}
+                                    required
+                                />
+
+                                <input
+                                    id="telefone"
+                                    type="text"
+                                    placeholder="Telefone:"
+                                    onChange={event => setTelefone(event.target.value)}
+                                    required
+                                />
+
+                                <input
+                                    id="setor"
+                                    type="text"
+                                    placeholder="Setor:"
+                                    onChange={event => setSetor(event.target.value)}
+                                    required
+                                />
+                            </div>
+
+                            <div>
+                                <input
+                                    id="rua"
+                                    type="text"
+                                    placeholder="Rua:"
+                                    onChange={event => setRua(event.target.value)}
+                                    required
+                                />
+
+                                <input
+                                    id="numero"
+                                    type="text"
+                                    placeholder="Numero:"
+                                    onChange={event => setNumero(event.target.value)}
+                                    required
+                                />
+
+                                <input
+                                    id="CEP"
+                                    type="text"
+                                    placeholder="CEP:"
+                                    onChange={event => setCEP(event.target.value)}
+                                    required
+                                />
+
+                                <input
+                                    id="cidade"
+                                    type="text"
+                                    placeholder="Cidade:"
+                                    onChange={event => setCidade(event.target.value)}
+                                    required
+                                />
+
+                                <input
+                                    id="estado"
+                                    type="text"
+                                    placeholder="Estado:"
+                                    onChange={event => setEstado(event.target.value)}
+                                    required
+                                />
+                            </div>
+                        </section>
+                    </form>
+                </div>
+
+                <footer>
+                    <button onClick={handleCreate}>Cadastrar</button>
+                </footer>
+            </section>
+        </main>
+    );
+}
