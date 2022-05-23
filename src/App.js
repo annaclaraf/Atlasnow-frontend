030cec57e101ab2c9cfcafec66424ae771e9f651
annaclaraf <anisferraz@gmail.com>
2022-05-23 12:57:20 -0300

    update routes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import { Login } from './pages/login/index'
 import { Home } from './pages/home/index'
 import { Funcionario } from './pages/funcionario/index'
 import { FuncionarioCadastro } from './pages/funcionario/cadastrar/index'
-import { FuncionarioEditar } from './pages/funcionario/editar/index'
-import {Emissor} from './pages/emissor/index'
-import {EmissorEditar} from './pages/emissor/editar/index'
-import {EmissorLista} from './pages/emissor/lista/index'
+import { FuncionarioEditar } from './pages/funcionario/editar/index'//
+import { FuncionarioView } from './pages/funcionario/view/index'
+import { Emissor } from './pages/emissor/index'
+import { EmissorEditar } from './pages/emissor/editar/index'
+import { EmissorView } from './pages/emissor/view/index'
+import { EmissorCadastro } from './pages/emissor/cadastrar/index'
 
 
 import './App.css'
@@ -16,19 +18,21 @@ import './App.css'
 function App() {
   return (
     <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Inicial />} />
-          <Route path="/home" element={<Home/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/funcionarios" element={<Funcionario/>} />
-          <Route path="/funcionarios/cadastro" element={<FuncionarioCadastro/>} />
-          <Route path="/funcionarios/editar" element={<FuncionarioEditar/>} />
-          <Route path="/emissor" element={<Emissor/>} />
-          <Route path="/emissor/editar" element={<EmissorEditar/>} />
-          <Route path="/emissor/lista" element={<EmissorLista/>} />
+      <Routes>
+        <Route exact path="/" element={<Inicial />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/funcionarios" element={<Funcionario />} />
+        <Route path="/funcionarios/cadastro" element={<FuncionarioCadastro />} />
+        <Route path="/funcionarios/editar" element={<FuncionarioEditar />} />
+        <Route path="/funcionarios/view" element={<FuncionarioView />} />        
+        <Route path="/emissor" element={<Emissor />} />
+        <Route path="/emissor/editar" element={<EmissorEditar />} />
+        <Route path="/emissor/view" element={<EmissorView />} />
+        <Route path="/emissor/cadastrar" element={<EmissorCadastro />} />
       </Routes>
     </BrowserRouter>
- );
+  );
 }
 
 export default App;
